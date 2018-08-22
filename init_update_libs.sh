8e438aad9024181d046655f537d3c9fe421571e2
test <test@test.test>
2018-08-22 22:03:09 +0200

    Update dependencies

--- init_update_libs.sh
+++ init_update_libs.sh
@@ -10,8 +10,8 @@ rm -rf expat-*
 rm -rf fribidi-*
 rm -rf lame-*
 
-wget -O- ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/libpng-1.6.21.tar.xz | tar xJ
-wget -O- http://downloads.sourceforge.net/project/expat/expat/2.1.0/expat-2.1.0.tar.gz | tar xz
-wget -O- http://fribidi.org/download/fribidi-0.19.7.tar.bz2 | tar xj
-wget -O- http://sourceforge.net/projects/lame/files/lame/3.99/lame-3.99.5.tar.gz | tar xz
+wget -O- ftp://ftp-osl.osuosl.org/pub/libpng/src/libpng16/libpng-1.6.35.tar.xz | tar xJ
+wget -O- https://github.com/libexpat/libexpat/releases/download/R_2_2_6/expat-2.2.6.tar.bz2 | tar xj
+wget -O- https://github.com/fribidi/fribidi/releases/download/v1.0.5/fribidi-1.0.5.tar.bz2 | tar xj
+wget -O- http://sourceforge.net/projects/lame/files/lame/3.100/lame-3.100.tar.gz | tar xz
 echo "============================================"
